db0ffc835557ce7ff1265f8383ce9bd0b61bd768
Nikita <torchhik_darkness@mail.ru>
2023-02-25 23:32:16 +0300

    Final Version_v2

--- work.py
+++ work.py
@@ -7,13 +7,9 @@ def open_dict(name):
             basic_dict[i] = len(a)
             sorted_files = sorted(basic_dict.items(),key=lambda item: item[1])
     for keys,values in sorted_files:
-        i = 0
-        d.writelines(keys)
-        d.writelines("\n")
-        d.writelines(str(values))
-        d.writelines("\n")
+        d.writelines(f'{keys}\n')
+        d.writelines(f'{str(values)}\n')
         for line in open(keys,encoding="UTF-8"):
-            i+=1
             d.writelines(line)
         d.writelines("\n")
 
