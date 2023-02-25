a322d5177407376735aa6b42f848358b95892c0a
Nikita <torchhik_darkness@mail.ru>
2023-02-25 23:25:20 +0300

    Final Version

--- work.py
+++ work.py
@@ -1,22 +1,26 @@
-def open_dict():
-    list_1 = ["1.txt","2.txt","3.txt"]
+def open_dict(name):
+    d = open("result.txt","w",encoding="UTF-8")
     basic_dict = {}
-    for i in list_1:
+    for i in name:
         with open(i,encoding="UTF-8") as f:
             a = f.readlines()
             basic_dict[i] = len(a)
             sorted_files = sorted(basic_dict.items(),key=lambda item: item[1])
     for keys,values in sorted_files:
         i = 0
-        print(keys)
-        print(values)
+        d.writelines(keys)
+        d.writelines("\n")
+        d.writelines(str(values))
+        d.writelines("\n")
         for line in open(keys,encoding="UTF-8"):
             i+=1
-            print(line,end = "")
-        print("\n",end = "")
-    
-        
-open_dict()
+            d.writelines(line)
+        d.writelines("\n")
+
+
+
+list_1 = ["1.txt","2.txt","3.txt"]        
+open_dict(list_1)
 
 
 
